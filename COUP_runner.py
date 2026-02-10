76bcbb57ada4852b23ab1c82c993ff64c9529573
skippybal <kasper.notebomer@hotmail.nl>
2026-02-10 14:53:55 +0100

    Update COUP_runner.py (non-functional)

--- COUP_runner.py
+++ COUP_runner.py
@@ -32,6 +32,8 @@ class COUP:
         self.train = train_paths
         self.test = test_paths
         self.config_space = config_space
+        self.all_data = {}
+        self.configs = {} # TODO: This could also be a list? probably...
 
     @staticmethod
     def choose_max(main_array, secondary_array):
@@ -47,6 +49,48 @@ class COUP:
     @staticmethod
     def coup_message(p, r, n_p, i_star, epsilon_star, UCB, LCB, m, k):
         return f"coup: phase p={p}. r={r}. n_p={n_p} configs sampled. i_star={i_star:5}, epsilon_star={epsilon_star:.4f}, ucb=[{np.min(UCB):.4f}, {np.max(UCB):.4f}], lcb=[{np.min(LCB):.4f}, {np.max(LCB):.4f}], m=[{min(m.values())}, {max(m.values())}], k=[{min(k.values())}, {max(k.values())}]"
+
+    def verify_instance(self, config_id, instance_id, cutoff, specifics="0", runlength="2147483647", seed="-1", runsolver_path='/home/skippybal/Projects/Styx/aclib2/configurators/smac/example_scenarios/spear-generic-wrapper/runsolver'):
+        # TODO: could also give flag: captime_increase, and then only do this loop as the instance id should exist
+        if instance_id in self.all_data[config_id]:
+            if self.all_data[config_id][instance_id][0] != "TIMEOUT":
+                return self.all_data[config_id][instance_id][0]
+
+        wrapped_runner = PipelineWrapper()
+        # instance_path = '/home/skippybal/Projects/THESIS/aclib2/instances/mip/data/SDPdMLPa-MIPVerify/mip_29.lp'
+        # instance_path = instance_loc
+        # specifics = '0'
+        # cutoff = '9600.0'
+        # runlength = '2147483647'
+        # seed = '-1'
+        start = ['--runsolver-path',
+                 runsolver_path,
+                 instance_path, specifics, cutoff, runlength, seed]
+
+        # sys.stdout.write(config)
+        variabls = []
+        # print(dict(config))
+        for variable, value in dict(self.configs[instance_id]).items():
+            # for variable, value in config.items():
+            variabls.append(f"-{variable}")
+            variabls.append(f'{value}')
+
+
+        all_args = start + variabls
+        wrapped_runner.main(all_args)
+
+        # Path("../storage/default").mkdir(parents=True, exist_ok=True)
+        with open(f"{STORAGE_LOC}/{instance_loc.split('/')[-1].split('.')[0][4:]}.txt", "w+") as f:
+            # print(f.read())
+            # f.write(f"{2}, {str(2)} \n")
+            f.write(
+                f"{wrapped_runner._ta_status}, {str(wrapped_runner._ta_runtime)}, {str(wrapped_runner._ta_runlength)},"
+                f"{str(wrapped_runner._ta_quality)}, {str(wrapped_runner._seed)}")
+        #TODO: instance_id or instance loc
+        #TODO: store in outer loop not here
+        return (config_id, instance_id, wrapped_runner._ta_status, str(wrapped_runner._ta_runtime),
+                str(wrapped_runner._ta_runlength), str(wrapped_runner._ta_quality), str(wrapped_runner._seed))
+
 
     @staticmethod
     def update_output(out, **kwargs):
@@ -100,6 +144,7 @@ class COUP:
                 F_hat[i] = 0
                 m[i] = 0
                 k[i] = k0
+                # TODO: sample new configs here
 
             ns.append(n_p)
 
@@ -134,17 +179,27 @@ class COUP:
                     dubcond = 2 * (1 - utility(k[i])) * alpha_i <= utility(k[i]) * (1 - F_hat[i] + alpha_i)
 
 
+                # TODO: after this it sould write to csv... but need to look into doulbess, or just write self.all_data to csv?
                 if dubcond:
                     k[i] = 2 * k[i]
                     #TODO: here we need to make sure we only rerun the ones that have t>k
 
-                    runtimes = [env.run(i, j, k[i]) for j in range(m[i])]
+                    # runtimes = [env.run(i, j, k[i]) for j in range(m[i])]
+                    runtimes = []
+                    for j in range(m[i]):
+                        config_id, instance_id, ta_status, runtime, ta_runlength, ta_quality, seed = self.verify_instance(i, m[i] - 1, k[i])
+                        #TODO: here store to all data
+                        # TODO: should this tuple also contain captime?
+                        self.all_data[config_id][instance_id] = (ta_status, runtime, ta_runlength, ta_quality, seed)
+                        runtimes.append(runtime)
 
                     F_hat[i] = sum([1 if t < k[i] else 0 for t in runtimes]) / m[i]
                     U_hat[i] = sum(utility(t) for t in runtimes) / m[i]
                 else: # otherwise, just run the next instance
 
-                    runtime = env.run(i, m[i] - 1, k[i])
+                    # runtime = env.run(i, m[i] - 1, k[i])
+                    config_id, instance_id, ta_status, runtime, ta_runlength, ta_quality, seed = self.verify_instance(i, m[i] - 1, k[i])
+                    self.all_data[config_id][instance_id] = (ta_status, runtime, ta_runlength, ta_quality, seed)
 
                     F_hat[i] = ((m[i] - 1) * F_hat[i] + (1 if runtime < k[i] else 0)) / m[i]
                     U_hat[i] = ((m[i] - 1) * U_hat[i] + utility(runtime)) / m[i]
@@ -171,6 +226,8 @@ class COUP:
                 if r % print_mod == 0:
                     print(self.coup_message(p, r, n_p, i_star, epsilon_star, UCB, LCB, m, k))
                 r += 1
+
+                # TODO: store entire object? then we also dont lose UCB and LCB... maybe a good idea?
 
             print(self.coup_message(p, r, n_p, i_star, epsilon_star, UCB, LCB, m, k) + ". PHASE {} COMPLETE.".format(p))
 
